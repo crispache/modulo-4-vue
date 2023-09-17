6a0b245329d86b90369e563fadc04e192898687b
crispache <cristinaglezpache06@gmail.com>
2023-09-17 17:55:31 +0100

    Añadir componente card y pintar los datos de los miembros

--- modulo-4-vue/pages/index.vue
+++ modulo-4-vue/pages/index.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>
-    <NuxtWelcome />
-    <v-btn color="primary">Start</v-btn>
-  </div>
+  <v-container>
+    <v-row>
+      <div v-for="user in users" :key="user.id">
+        <v-col cols="10">
+          <Card 
+            :organization-name="default_organization" 
+            :user-name="user.login" 
+            :photo-url="user.avatar_url" />
+        </v-col>
+      </div>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup lang="ts">
-import { githubService } from '~/apis/github';
+import { githubService } from "~/apis/github";
+import { UserGitHub } from "~/types/User";
 
-const users =  await githubService.getUsers('Lemoncode')
-console.log('Users', users)
+const default_organization = 'Lemoncode'
+
+const { getUsers, users } = useGithub();
+await getUsers(default_organization);
+
+console.log("Users", users);
 </script>
